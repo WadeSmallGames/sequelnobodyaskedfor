f895a61f3fb986261d04b6a8011631ea5acb8697
Wade Small <102333259+WadeSmallGames@users.noreply.github.com>
2023-12-18 23:59:09 +1030

    FPS Character Controller

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    private float xMousePos, smoothedMousePos, currentLookingPos;
+    public float smoothing = 1.5f, sensitivity = 1.5f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    public void Update()
+    {
+        GetInput();
+        ModifyInput();
+        MovePlayer();
+    }
+
+    public void GetInput()
+    {
+        xMousePos = Input.GetAxisRaw("Mouse X");
+    }
+    public void ModifyInput()
+    {
+        xMousePos *= sensitivity * smoothing;
+        smoothedMousePos = Mathf.Lerp(smoothedMousePos, xMousePos, 1f/smoothing);
+
+    }
+    public void MovePlayer()
+    {
+        currentLookingPos += smoothedMousePos;
+        transform.localRotation = Quaternion.AngleAxis(currentLookingPos, transform.up);
+    }
+
+}
---
+++ Assets/Scripts/FpsController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FpsController : MonoBehaviour
+{
+    public float speed = 20;
+    private CharacterController cc;
+
+    public Vector3 inputVector;
+    public Vector3 movementVector;
+    public float gravity = -10;
+    private Animator animator;
+    private bool isMoving;
+    public float magni;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        MovePlayer();
+        CheckForBob();
+        magni = cc.velocity.magnitude;
+        animator.SetBool("Moving", isMoving);
+
+        
+    }
+
+    void GetInput()
+    {
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        inputVector.Normalize();
+        inputVector = transform.TransformDirection(inputVector);
+
+        movementVector = (inputVector * speed) + (Vector3.up * gravity);    
+    }
+
+    void MovePlayer()
+    {
+        cc.Move(movementVector * Time.deltaTime);
+        
+    }
+
+    void CheckForBob()
+    {
+        if (cc.velocity.magnitude == 0)
+        {
+
+            isMoving = false;
+
+        }
+        else
+        {
+            isMoving = true;
+        }
+    }
+}
